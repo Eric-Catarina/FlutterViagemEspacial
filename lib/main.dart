5aef5bd4cf726d266f402c643d011960af1bace6
Eric-Catarina <2020954200@teiacoltec.org>
2022-08-30 11:50:51 -0300

    Primeira Versão

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main () => runApp(MeuApp());
+class MeuApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'Exploration!',
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      home: MinhaHomePage(),
+    );
+  }
+}
+
+class MinhaHomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Planejador de Viajem Espacial"),
+      ),
+      body: Column(
+        children: [
+          Progresso(),
+          ListaDeTarefas()
+        ],
+      ),
+    );
+  }
+}
+class Progresso extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Column(
+      children: [
+        Text("Você Já percorreu tudo isso do Universo"),
+        LinearProgressIndicator(value: 0.0),
+      ],
+    );
+  }
+}
+class ListaDeTarefas extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Column(
+      children: [
+        ItemDaLista(label: "Colocar recursos no foguete"),
+        ItemDaLista(label: "Lançar o Foguete"),
+        ItemDaLista(label: "Dar uma volta na Terra"),
+        ItemDaLista(label: "Ir em direção a Lua"),
+        ItemDaLista(label: "Chegar na Lua !"),
+      ],
+    );
+  }
+}
+
+class ItemDaLista extends StatelessWidget{
+  final String label;
+
+  const ItemDaLista({Key? key, required this.label}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Row(
+      children: [
+        Checkbox(value: false, onChanged: null),
+        Text(label),
+      ],
+    );
+  }
+}
